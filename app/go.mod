4abc5c69ee814c3b25d41f7eb3837d54edd87871
xm1k3 <73166077+xm1k3@users.noreply.github.com>
2024-09-09 23:59:15 +0200

    feat: used gocloak

--- app/go.mod
+++ app/go.mod
@@ -3,11 +3,19 @@ module github.com/dedoai/ddai-api-user
 go 1.23
 
 require (
+	github.com/Nerzal/gocloak/v12 v12.0.0
 	github.com/aws/aws-lambda-go v1.47.0
 	github.com/pquerna/otp v1.4.0
 )
 
 require (
 	github.com/boombuler/barcode v1.0.1-0.20190219062509-6c824513bacc // indirect
+	github.com/go-resty/resty/v2 v2.7.0 // indirect
+	github.com/golang-jwt/jwt/v4 v4.4.2 // indirect
+	github.com/golang-jwt/jwt/v5 v5.2.1
+	github.com/opentracing/opentracing-go v1.2.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/segmentio/ksuid v1.0.4 // indirect
 	github.com/stretchr/testify v1.8.1 // indirect
+	golang.org/x/net v0.0.0-20221019024206-cb67ada4b0ad // indirect
 )
--- app/main.go
+++ app/main.go
@@ -1,24 +1,35 @@
 package main
 
 import (
-	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
-	"io"
-	"net/http"
-	"net/url"
 	"os"
 	"strings"
 
+	"github.com/Nerzal/gocloak/v12"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	jwt5 "github.com/golang-jwt/jwt/v5"
 	"github.com/pquerna/otp/totp"
 )
 
-func Handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	// fmt.Printf("Received request: %+v\n", request)
+var (
+	client       gocloak.GoCloak
+	realm        string
+	clientID     string
+	clientSecret string
+)
 
+func init() {
+	realm = os.Getenv("KEYCLOAK_REALM")
+	clientID = "web-app"
+	clientSecret = os.Getenv("CLIENT_SECRET")
+
+	client = *gocloak.NewClient("https://sso.dev.dedoai.org")
+}
+
+func Handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	switch request.HTTPMethod {
 	case "GET":
 		if strings.HasPrefix(request.Path, "/account/") {
@@ -46,50 +57,48 @@ func Handler(ctx context.Context, request events.APIGatewayProxyRequest) (events
 }
 
 func handleGetUserProfile(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	fmt.Println("Handling GET user profile request")
-
 	username := strings.TrimPrefix(request.Path, "/account/")
 	if username == "" {
-		fmt.Println("Username is required")
 		return events.APIGatewayProxyResponse{
 			StatusCode: 400,
 			Body:       `{"error": "Username is required"}`,
 		}, nil
 	}
 
-	accessToken, err := getKeycloakAdminToken()
+	token, err := client.LoginAdmin(context.Background(), os.Getenv("KEYCLOAK_ADMIN_USERNAME"), os.Getenv("KEYCLOAK_ADMIN_PASSWORD"), realm)
 	if err != nil {
-		fmt.Printf("Failed to get admin token: %v\n", err)
 		return events.APIGatewayProxyResponse{
 			StatusCode: 500,
 			Body:       `{"error": "Failed to get admin token"}`,
 		}, nil
 	}
 
-	user, err := getUserByEmail(accessToken, username)
+	users, err := client.GetUsers(
+		context.Background(),
+		token.AccessToken,
+		realm,
+		gocloak.GetUsersParams{
+			Email: gocloak.StringP(username),
+		},
+	)
 	if err != nil {
-		fmt.Printf("Failed to get user: %v\n", err)
 		return events.APIGatewayProxyResponse{
 			StatusCode: 500,
 			Body:       fmt.Sprintf(`{"error": "Failed to get user: %v"}`, err.Error()),
 		}, nil
 	}
 
-	fmt.Printf("User profile retrieved: %+v\n", user)
-	return respondWithJSON(user, 200)
-}
-
-func handleLogin(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	fmt.Println("Handling login request")
-
-	if request.HTTPMethod != "POST" {
-		fmt.Println("Method not allowed")
+	if len(users) == 0 {
 		return events.APIGatewayProxyResponse{
-			StatusCode: 405,
-			Body:       `{"error": "Method not allowed"}`,
+			StatusCode: 404,
+			Body:       `{"error": "User not found"}`,
 		}, nil
 	}
 
+	return respondWithJSON(users[0], 200)
+}
+
+func handleLogin(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	var credentials struct {
 		Username string `json:"username"`
 		Password string `json:"password"`
@@ -97,86 +106,73 @@ func handleLogin(request events.APIGatewayProxyRequest) (events.APIGatewayProxyR
 	}
 	err := json.Unmarshal([]byte(request.Body), &credentials)
 	if err != nil {
-		fmt.Printf("Invalid request body: %v\n", err)
 		return events.APIGatewayProxyResponse{
 			StatusCode: 400,
 			Body:       `{"error": "Invalid request body"}`,
 		}, nil
 	}
 
-	realm := os.Getenv("KEYCLOAK_REALM")
-	clientSecret := os.Getenv("CLIENT_SECRET")
-
-	form := url.Values{}
-	form.Set("client_id", "web-app")
-	form.Set("client_secret", clientSecret)
-	form.Set("grant_type", "password")
-	form.Set("username", credentials.Username)
-	form.Set("password", credentials.Password)
-
-	req, err := http.NewRequest("POST", fmt.Sprintf("https://sso.dev.dedoai.org/realms/%s/protocol/openid-connect/token", realm), strings.NewReader(form.Encode()))
+	jwt, err := client.Login(
+		context.Background(),
+		clientID,
+		clientSecret,
+		realm,
+		credentials.Username,
+		credentials.Password,
+	)
 	if err != nil {
-		fmt.Printf("Failed to create request: %v\n", err)
 		return events.APIGatewayProxyResponse{
-			StatusCode: 500,
-			Body:       `{"error": "Failed to create request"}`,
-		}, nil
-	}
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Printf("Failed to get token: %v\n", err)
-		return events.APIGatewayProxyResponse{
-			StatusCode: 500,
-			Body:       `{"error": "Failed to get token"}`,
-		}, nil
-	}
-	defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Printf("Failed to read response: %v\n", err)
-		return events.APIGatewayProxyResponse{
-			StatusCode: 500,
-			Body:       `{"error": "Failed to read response"}`,
-		}, nil
-	}
-
-	var tokenResponse struct {
-		AccessToken string `json:"access_token"`
-	}
-	err = json.Unmarshal(body, &tokenResponse)
-	if err != nil {
-		fmt.Printf("Failed to parse token response: %v\n", err)
-		return events.APIGatewayProxyResponse{
-			StatusCode: 500,
-			Body:       `{"error": "Failed to parse token response"}`,
+			StatusCode: 401,
+			Body:       `{"error": "Invalid credentials"}`,
 		}, nil
 	}
 
 	if credentials.OTPToken == "" {
-		fmt.Println("OTP token is missing")
 		return events.APIGatewayProxyResponse{
 			StatusCode: 400,
 			Body:       `{"error": "OTP token is missing"}`,
 		}, nil
 	}
 
-	user, err := getUserByEmail(tokenResponse.AccessToken, credentials.Username)
+	fmt.Printf("%+v\n", jwt.AccessToken)
+
+	token, _ := jwt5.Parse(jwt.AccessToken, func(token *jwt5.Token) (interface{}, error) {
+		return nil, nil
+	})
+
+	claims, ok := token.Claims.(jwt5.MapClaims)
+	if !ok {
+		return events.APIGatewayProxyResponse{
+			StatusCode: 500,
+			Body:       `{"error": "Failed to extract JWT claims"}`,
+		}, nil
+	}
+
+	sub, ok := claims["sub"].(string)
+	if !ok {
+		return events.APIGatewayProxyResponse{
+			StatusCode: 500,
+			Body:       `{"error": "Failed to extract 'sub' claim from JWT token"}`,
+		}, nil
+	}
+
+	user, err := client.GetUserByID(
+		context.Background(),
+		jwt.AccessToken,
+		realm,
+		sub,
+	)
 	if err != nil {
-		fmt.Printf("Failed to get user: %v\n", err)
 		return events.APIGatewayProxyResponse{
 			StatusCode: 500,
 			Body:       `{"error": "Failed to get user"}`,
 		}, nil
 	}
 
-	otpSecret := user["attributes"].(map[string]interface{})["otp_secret"].([]interface{})[0].(string)
+	attributes := *user.Attributes
+	otpSecret := attributes["otp_secret"][0]
 	valid := totp.Validate(credentials.OTPToken, otpSecret)
 	if !valid {
-		fmt.Println("Invalid OTP token")
 		return events.APIGatewayProxyResponse{
 			StatusCode: 401,
 			Body:       `{"error": "Invalid OTP token"}`,
@@ -185,21 +181,11 @@ func handleLogin(request events.APIGatewayProxyRequest) (events.APIGatewayProxyR
 
 	return respondWithJSON(map[string]string{
 		"status":       "success",
-		"access_token": tokenResponse.AccessToken,
+		"access_token": jwt.AccessToken,
 	}, 200)
 }
 
 func handleRegister(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	fmt.Println("Handling registration request")
-
-	if request.HTTPMethod != "POST" {
-		fmt.Println("Method not allowed")
-		return events.APIGatewayProxyResponse{
-			StatusCode: 405,
-			Body:       `{"error": "Method not allowed"}`,
-		}, nil
-	}
-
 	var userData struct {
 		Username  string `json:"username"`
 		Email     string `json:"email"`
@@ -210,16 +196,14 @@ func handleRegister(request events.APIGatewayProxyRequest) (events.APIGatewayPro
 	}
 	err := json.Unmarshal([]byte(request.Body), &userData)
 	if err != nil {
-		fmt.Printf("Invalid request body: %v\n", err)
 		return events.APIGatewayProxyResponse{
 			StatusCode: 400,
 			Body:       `{"error": "Invalid request body"}`,
 		}, nil
 	}
 
-	accessToken, err := getKeycloakAdminToken()
+	token, err := client.LoginAdmin(context.Background(), os.Getenv("KEYCLOAK_ADMIN_USERNAME"), os.Getenv("KEYCLOAK_ADMIN_PASSWORD"), realm)
 	if err != nil {
-		fmt.Printf("Failed to get admin token: %v\n", err)
 		return events.APIGatewayProxyResponse{
 			StatusCode: 500,
 			Body:       `{"error": "Failed to get admin token"}`,
@@ -231,23 +215,52 @@ func handleRegister(request events.APIGatewayProxyRequest) (events.APIGatewayPro
 		AccountName: userData.Email,
 	})
 	if err != nil {
-		fmt.Printf("Failed to generate OTP secret: %v\n", err)
 		return events.APIGatewayProxyResponse{
 			StatusCode: 500,
 			Body:       `{"error": "Failed to generate OTP secret"}`,
 		}, nil
 	}
 
-	userID, err := createKeycloakUser(accessToken, key.Secret(), userData)
+	user := gocloak.User{
+		Username:  gocloak.StringP(userData.Email),
+		Email:     gocloak.StringP(userData.Email),
+		FirstName: gocloak.StringP(userData.FirstName),
+		LastName:  gocloak.StringP(userData.LastName),
+		Enabled:   gocloak.BoolP(true),
+		Attributes: &map[string][]string{
+			"phone":      {userData.Phone},
+			"otp_secret": {key.Secret()},
+		},
+	}
+
+	userID, err := client.CreateUser(
+		context.Background(),
+		token.AccessToken,
+		realm,
+		user,
+	)
 	if err != nil {
-		fmt.Printf("Failed to create user: %v\n", err)
 		return events.APIGatewayProxyResponse{
 			StatusCode: 500,
 			Body:       fmt.Sprintf(`{"error": "Failed to create user: %v"}`, err.Error()),
 		}, nil
 	}
 
-	fmt.Printf("User created successfully, user ID: %s\n", userID)
+	err = client.SetPassword(
+		context.Background(),
+		token.AccessToken,
+		userID,
+		realm,
+		userData.Password,
+		false,
+	)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			StatusCode: 500,
+			Body:       fmt.Sprintf(`{"error": "Failed to set password: %v"}`, err.Error()),
+		}, nil
+	}
+
 	return respondWithJSON(map[string]interface{}{
 		"status":     "success",
 		"user_id":    userID,
@@ -256,174 +269,15 @@ func handleRegister(request events.APIGatewayProxyRequest) (events.APIGatewayPro
 	}, 201)
 }
 
-func getKeycloakAdminToken() (string, error) {
-	fmt.Println("Getting Keycloak admin token")
-
-	adminUsername := os.Getenv("KEYCLOAK_ADMIN_USERNAME")
-	adminPassword := os.Getenv("KEYCLOAK_ADMIN_PASSWORD")
-	realm := os.Getenv("KEYCLOAK_REALM")
-
-	requestBody := url.Values{}
-	requestBody.Set("username", adminUsername)
-	requestBody.Set("password", adminPassword)
-	requestBody.Set("grant_type", "password")
-	requestBody.Set("client_id", "admin-cli")
-
-	req, err := http.NewRequest("POST", fmt.Sprintf("https://sso.dev.dedoai.org/realms/%s/protocol/openid-connect/token", realm), strings.NewReader(requestBody.Encode()))
-	if err != nil {
-		fmt.Printf("Failed to create request: %v\n", err)
-		return "", err
-	}
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Printf("Failed to get admin token: %v\n", err)
-		return "", err
-	}
-	defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Printf("Failed to read response: %v\n", err)
-		return "", err
-	}
-
-	var tokenResponse struct {
-		AccessToken string `json:"access_token"`
-	}
-	err = json.Unmarshal(body, &tokenResponse)
-	if err != nil {
-		fmt.Printf("Failed to parse token response: %v, body: %s\n", err, string(body))
-		return "", fmt.Errorf("failed to parse token response: %v", string(body))
-	}
-
-	fmt.Println("Admin token retrieved successfully")
-	return tokenResponse.AccessToken, nil
-}
-
-func createKeycloakUser(accessToken string, otpSecret string, userData struct {
-	Username  string `json:"username"`
-	Email     string `json:"email"`
-	FirstName string `json:"firstName"`
-	LastName  string `json:"lastName"`
-	Phone     string `json:"phone"`
-	Password  string `json:"password"`
-}) (string, error) {
-	fmt.Println("Creating Keycloak user")
-
-	realm := os.Getenv("KEYCLOAK_REALM")
-
-	userPayload := map[string]interface{}{
-		"username":  userData.Email,
-		"email":     userData.Email,
-		"firstName": userData.FirstName,
-		"lastName":  userData.LastName,
-		"attributes": map[string][]string{
-			"phone":      {userData.Phone},
-			"otp_secret": {otpSecret},
-		},
-		"enabled": true,
-		"credentials": []map[string]interface{}{
-			{
-				"type":      "password",
-				"value":     userData.Password,
-				"temporary": false,
-			},
-		},
-	}
-	payloadBytes, err := json.Marshal(userPayload)
-	if err != nil {
-		fmt.Printf("Failed to marshal user payload: %v\n", err)
-		return "", err
-	}
-
-	req, err := http.NewRequest("POST", fmt.Sprintf("https://sso.dev.dedoai.org/admin/realms/%s/users", realm), bytes.NewBuffer(payloadBytes))
-	if err != nil {
-		fmt.Printf("Failed to create request: %v\n", err)
-		return "", err
-	}
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", "Bearer "+accessToken)
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Printf("Failed to create user: %v\n", err)
-		return "", err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusCreated {
-		body, _ := io.ReadAll(resp.Body)
-		fmt.Printf("Failed to create user, status code: %d, response: %s\n", resp.StatusCode, string(body))
-		return "", fmt.Errorf("failed to create user, status code: %d, response: %s", resp.StatusCode, string(body))
-	}
-
-	location := resp.Header.Get("Location")
-	userID := location[strings.LastIndex(location, "/")+1:]
-
-	fmt.Printf("User created successfully, user ID: %s\n", userID)
-	return userID, nil
-}
-
-func getUserByEmail(accessToken, email string) (map[string]interface{}, error) {
-	realm := os.Getenv("KEYCLOAK_REALM")
-
-	req, err := http.NewRequest("GET", fmt.Sprintf("https://sso.dev.dedoai.org/admin/realms/%s/users?email=%s", realm, url.QueryEscape(email)), nil)
-	if err != nil {
-		fmt.Printf("Failed to create request: %v\n", err)
-		return nil, err
-	}
-	req.Header.Set("Authorization", "Bearer "+accessToken)
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Printf("Failed to get user: %v\n", err)
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	fmt.Printf("Response Status Code: %d\n", resp.StatusCode)
-	fmt.Printf("Response Headers: %v\n", resp.Header)
-
-	body, _ := io.ReadAll(resp.Body)
-	fmt.Printf("Response Body: %s\n", string(body))
-
-	if resp.StatusCode != http.StatusOK {
-		fmt.Printf("Failed to get user, status code: %d, response: %s\n", resp.StatusCode, string(body))
-		return nil, fmt.Errorf("failed to get user, status code: %d, response: %s", resp.StatusCode, string(body))
-	}
-
-	var users []map[string]interface{}
-	err = json.Unmarshal(body, &users)
-	if err != nil {
-		fmt.Printf("Failed to decode user response: %v\n", err)
-		return nil, err
-	}
-
-	if len(users) == 0 {
-		fmt.Printf("User not found: %s\n", email)
-		return nil, fmt.Errorf("user not found")
-	}
-
-	fmt.Printf("User retrieved successfully: %+v\n", users[0])
-	return users[0], nil
-}
-
 func respondWithJSON(data interface{}, statusCode int) (events.APIGatewayProxyResponse, error) {
 	body, err := json.Marshal(data)
 	if err != nil {
-		fmt.Printf("Failed to marshal response body: %v\n", err)
 		return events.APIGatewayProxyResponse{
 			StatusCode: 500,
 			Body:       `{"error": "Internal Server Error"}`,
 		}, nil
 	}
 
-	fmt.Printf("Responding with JSON: %s\n", string(body))
 	return events.APIGatewayProxyResponse{
 		StatusCode: statusCode,
 		Headers: map[string]string{
